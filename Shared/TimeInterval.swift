cccca3ce7d641a33cfe2190c5e51b7db72c49974
Marco Boschi <boschi95@gmail.com>
2017-03-23 15:27:18 +0100

    - Removed cast to integer to avoid overflow error

--- Shared/TimeInterval.swift
+++ Shared/TimeInterval.swift
@@ -11,24 +11,24 @@ import Foundation
 extension TimeInterval {
 	
 	public func getDuration(hideHours shouldHide: Bool = false) -> String {
-		var s = Int(floor(self))
+		var s = self
 		let neg = s < 0
 		if neg {
 			s *= -1
 		}
 		
-		var m = s / 60
-		s = s % 60
+		let m = floor(s / 60)
+		let sec = Int(fmod(s, 60))
 		
-		let h = m / 60
-		m = m % 60
+		let h = floor(m / 60)
+		let min = Int(fmod(m, 60))
 		let doHide = shouldHide && h == 0
 		
-		let sec = (s < 10 ? "0" : "") + "\(s)"
-		let min = (m < 10 && !doHide ? "0" : "") + "\(m)"
-		let hour = doHide ? "" : "\(h):"
+		var res = (sec < 10 ? "0" : "") + "\(sec)"
+		res = (min < 10 && !doHide ? "0" : "") + "\(min):" + res
+		res = (doHide ? "" : "\(h.toString()):") + res
 		
-		return (neg ? "-" : "") + "\(hour)\(min):\(sec)"
+		return (neg ? "-" : "") + res
 	}
 	
 	public func getUNIXDateTime() -> String {
