83835fbc9a52bdf44e1aa6798f786560965c355d
Marco Boschi <boschi95@gmail.com>
2018-07-31 21:46:29 +0200

    - Reverted animation change for macOS

--- Shared/Main iOS & macOS.swift
+++ Shared/Main iOS & macOS.swift
@@ -11,7 +11,13 @@ import QuartzCore
 
 public let shakingAnimation: CAKeyframeAnimation = {
 	let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+	
+	#if os(macOS)
+	animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+	#else
 	animation.timingFunction = CAMediaTimingFunction(name: .linear)
+	#endif
+	
 	animation.duration = 0.4
 	
 	animation.values = [-10, 10, -10, 10, -5, 5, -2.5, 2.5, 0]
