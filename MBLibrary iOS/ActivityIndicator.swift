77da6f1f4cab574fede78ee507e01f90cca86a37
Marco Boschi <boschi95@gmail.com>
2019-06-09 22:41:21 +0200

    - Expose UIActivityIndicatorView to SwiftUI - Duration string now is provided in localized form and raw form, the latter is equivalent to the old string but forces hours to be always visible

---
+++ MBLibrary iOS/ActivityIndicator.swift
@@ -0,0 +1,43 @@
+//
+//  ActivityIndicator.swift
+//  MBLibrary iOS
+//
+//  Created by Marco Boschi on 09/06/2019.
+//  Copyright © 2019 Marco Boschi. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+@available(iOSApplicationExtension 13.0, *)
+public struct ActivityIndicator : UIViewRepresentable {
+	public let style: UIActivityIndicatorView.Style
+	private let animating: Binding<Bool>
+
+	public init(style: UIActivityIndicatorView.Style, animating: Binding<Bool>) {
+		self.style = style
+		self.animating = animating
+	}
+
+	public func makeUIView(context: Context) -> UIActivityIndicatorView {
+		let indicator = UIActivityIndicatorView(style: style)
+
+		return indicator
+	}
+
+	public func updateUIView(_ indicator: UIActivityIndicatorView, context: Context) {
+		if animating.value {
+			indicator.startAnimating()
+		} else {
+			indicator.stopAnimating()
+		}
+	}
+}
+
+#if DEBUG
+struct ActivityIndicator_Previews : PreviewProvider {
+    static var previews: some View {
+        ActivityIndicator()
+    }
+}
+#endif
